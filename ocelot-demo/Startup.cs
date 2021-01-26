caf15e750d5a2166b94597f639a81ad4c70a83c1
Umutcan Alpak <umutcan.alpak@alpata.com>
2021-01-26 18:17:49 +0300

    ocelot configuration

--- ocelot-demo/Startup.cs
+++ ocelot-demo/Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +18,7 @@ namespace ocelot_demo
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddOcelot();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +38,8 @@ namespace ocelot_demo
                     await context.Response.WriteAsync("Hello World!");
                 });
             });
+
+            app.UseOcelot().Wait();
         }
     }
 }
