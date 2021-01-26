087d702ec33c832f91a5a0fc184456a9a7335081
Umutcan Alpak <umutcan.alpak@alpata.com>
2021-01-26 19:38:02 +0300

    route to other api

--- ocelot-demo/Program.cs
+++ ocelot-demo/Program.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace ocelot_demo
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
@@ -20,7 +20,9 @@ namespace ocelot_demo
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.ConfigureAppConfiguration(config => config.AddJsonFile($"ocelot.{env}.json"));
                 })
                 .ConfigureLogging(logging => logging.AddConsole());
     }
